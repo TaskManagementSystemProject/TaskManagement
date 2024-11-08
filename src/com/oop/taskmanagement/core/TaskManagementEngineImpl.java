1f82a6773401bfa832a656e9f9b5485b032f0b69
Danny <jordantsolov@gmail.com>
2024-11-08 19:01:07 +0200

    refactor engine process command logic

--- src/com/oop/taskmanagement/core/TaskManagementEngineImpl.java
+++ src/com/oop/taskmanagement/core/TaskManagementEngineImpl.java
@@ -13,7 +13,8 @@ import java.util.Scanner;
 public class TaskManagementEngineImpl implements TaskManagementEngine {
     private static final String TERMINATION_COMMAND = "Exit";
     private static final String EMPTY_COMMAND_ERROR = "Command cannot be empty.";
-    private static final String MAIN_SPLIT_SYMBOL = " ";
+    private static final char MAIN_SPLIT_SYMBOL = ' ';
+    private static final char SENTENCE_SEPARATOR = '^';
     private static final String TEXT_OPEN_SYMBOL = "<<";
     private static final String TEXT_CLOSE_SYMBOL = ">>";
     private static final String REPORT_SEPARATOR = "####################";
@@ -50,85 +51,97 @@ public class TaskManagementEngineImpl implements TaskManagementEngine {
     }
 
     private void processCommand(String inputLine) {
-        String commandName = extractCommandName(inputLine);
-        List<String> parameters = extractCommandParameters(inputLine);
+        List<String> allArguments = extractAllArguments(inputLine);
+        String commandName = extractCommandName(allArguments);
+        List<String> parameters = extractCommandParameters(allArguments);
         Command command = commandFactory.createCommandFromCommandName(commandName, taskManagementRepository);
         String executionResult = command.execute(parameters);
         print(executionResult);
     }
 
     /**
-     * Receives a full line and extracts the command to be executed from it.
-     * For example, if the input line is "FilterBy Assignee John", the method will return "FilterBy".
+     * Receives a list of all parameters and extracts the command to be executed from it.
+     * For example, if the input list is ["FilterBy", "Assignee", "John"] the method will return "FilterBy".
      *
-     * @param inputLine A complete input line
+     * @param allParameters A list of all parameters
      * @return The name of the command to be executed
      */
-    private String extractCommandName(String inputLine) {
-        return inputLine.split(" ")[0];
+    private String extractCommandName(List<String> allParameters) {
+        return allParameters.get(0);
     }
 
     /**
-     * Receives a full line and extracts the parameters that are needed for the command to execute.
-     * For example, if the input line is "FilterBy Assignee John",
+     * Receives a full list of arguments and extracts the parameters that are needed for the command to execute.
+     * For example, if the input list is ["FilterBy", "Assignee", "John"]
      * the method will return a list of ["Assignee", "John"].
      *
-     * @param inputLine A complete input line
+     * @param inputLine A list of all parameters
      * @return A list of the parameters needed to execute the command
      */
-    private List<String> extractCommandParameters(String inputLine) {
-        if (inputLine.contains(TEXT_OPEN_SYMBOL)) {
-            return extractTextParameters(inputLine);
-        }
-        String[] commandParts = inputLine.split(" ");
+    private List<String> extractCommandParameters(List<String> inputLine) {
         List<String> parameters = new ArrayList<>();
-        for (int i = 1; i < commandParts.length; i++) {
-            parameters.add(commandParts[i]);
+        for (int i = 1; i < inputLine.size(); i++) {
+            parameters.add(inputLine.get(i));
         }
         return parameters;
     }
 
-    /**
-     * Receives a full command in which there are fields that contain multiple words (a.k.a sentence) and rework those specific
-     * parameters to a single String which is afterward passed as parameters for the command to execute.
-     * For example, if the input like is CreateTask {{This is a specific task}} {{When I click there, that happens}} ..
-     * the method will return a list of ["This is a specific task", "When I click there, that happens", ..]
-     * @param fullCommand A complete command
-     * @return A list of parameters needed to execute the command
-     */
-    public List<String> extractTextParameters(String fullCommand) {
-        int indexOfFirstSeparator = fullCommand.indexOf(MAIN_SPLIT_SYMBOL);
-        int indexOfOpenText = fullCommand.indexOf(TEXT_OPEN_SYMBOL);
-        int indexOfCloseText = fullCommand.indexOf(TEXT_CLOSE_SYMBOL);;
-        List<String> parameters = new ArrayList<>();
-
-        // add the Title input text as a parameter // or in case of addingComment it takes just the comment
-        fullCommand = getString(fullCommand, parameters, indexOfOpenText, indexOfCloseText);
-
-        indexOfOpenText = fullCommand.indexOf(TEXT_OPEN_SYMBOL);
-        indexOfCloseText = fullCommand.indexOf(TEXT_CLOSE_SYMBOL);;
-        if (indexOfOpenText != - 1) {
-            // if there are two times {{}} {{}} in the fullCommand -> gets both of the strings included
-            // if there are more than 1 strings included, the case is when creating a task -> has title and description, we are taking the description
-            // as well as a parameter
-            fullCommand = getString(fullCommand, parameters, indexOfOpenText, indexOfCloseText);
-        }
-
-        List<String> result = new ArrayList<>(Arrays.asList(fullCommand.substring(indexOfFirstSeparator + 1).split(MAIN_SPLIT_SYMBOL)));
-        parameters.addAll(result);
-        return parameters;
-    }
-
-    // adds the parameter in the OPEN/CLOSE symbol as a single String and removes from the fullCommand String.
-    private String getString(String fullCommand, List<String> parameters, int indexOfOpenText, int indexOfCloseText) {
-        parameters.add(fullCommand.substring(indexOfOpenText + TEXT_OPEN_SYMBOL.length(), indexOfCloseText));
-        fullCommand = fullCommand.replaceFirst("<<.+?>> ", "");
-        return fullCommand;
-    }
 
     private void print(String result) {
         System.out.println(result);
         System.out.println(REPORT_SEPARATOR);
     }
 
+    /**
+     * Receives a full line and extracts the parameters that are needed for the command to execute.
+     * For example, if the input line is "FilterBy Assignee ^John Jones^",
+     * the method will return a list of ["FilterBy", "Assignee", "John Jones"].
+     *
+     * @param lineToBeSplit A complete input line
+     * @return A list of the parameters needed to execute the command
+     */
+    private List<String> extractAllArguments(String lineToBeSplit) {
+        StringBuilder currentWord = new StringBuilder();
+        boolean insideBracket = false;
+        ArrayList<String> result = new ArrayList<>();
+
+        for (char symbol : lineToBeSplit.toCharArray()) {
+            // if insideBracket // dependable on SENTENCE_SEPARATOR, does not count space as a special symbol. Adds the element inside the result array
+            // once we see the closing separator
+            if (insideBracket) {
+                if (symbol != SENTENCE_SEPARATOR) {
+                    currentWord.append(symbol);
+                    continue;
+                }
+                if (!currentWord.isEmpty()) {
+                    result.add(currentWord.toString());
+                    currentWord.setLength(0);
+                }
+                insideBracket = false;
+            } else {
+                // if we are outside of the bracket ( text that has to be considered as whole ) we are dealing with either a single word parameter or text
+                // that we handle by catching the different scenarios// if its SENTENCE_SEPARATOR it means we are going inside brackets
+                // if its a space we are adding the word only if the currentWord is not empty -> handles the case where multiple spaces are used
+                // or if there is a single word parameter after another single word parameter.
+                switch (symbol) {
+                    case SENTENCE_SEPARATOR:
+                        insideBracket = true;
+                        break;
+                    case MAIN_SPLIT_SYMBOL:
+                        if (!currentWord.isEmpty()) {
+                            result.add(currentWord.toString());
+                            currentWord.setLength(0);
+                        }
+                        break;
+                    default:
+                        currentWord.append(symbol);
+                }
+            }
+        }
+        // in case we end with a single word parameter, add to the list;
+        if (!currentWord.isEmpty()) {
+            result.add(currentWord.toString());
+        }
+        return result;
+    }
 }
