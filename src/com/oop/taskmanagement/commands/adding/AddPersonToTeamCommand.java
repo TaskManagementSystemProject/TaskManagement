364eb14b9ed760f2a33973a89204391334ba4558
Danny <jordantsolov@gmail.com>
2024-11-09 19:05:36 +0200

    implement AddPersonToTeamCommand

--- src/com/oop/taskmanagement/commands/adding/AddPersonToTeamCommand.java
+++ src/com/oop/taskmanagement/commands/adding/AddPersonToTeamCommand.java
@@ -2,11 +2,17 @@ package com.oop.taskmanagement.commands.adding;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.models.contracts.team.Member;
+import com.oop.taskmanagement.models.contracts.team.Team;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class AddPersonToTeamCommand implements Command {
 
+    private final static String MEMBER_ADDED_SUCCESSFULLY = "Member %s added to team %s successfully.";
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
     private final TaskManagementRepository taskManagementRepository;
 
     public AddPersonToTeamCommand(TaskManagementRepository taskManagementRepository) {
@@ -15,7 +21,14 @@ public class AddPersonToTeamCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
-        return "";
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        String memberName = parameters.get(0);
+        String teamName = parameters.get(1);
+        Member memberToAdd = taskManagementRepository.findMemberByName(memberName);
+        Team teamForAdding = taskManagementRepository.findTeamByName(teamName);
+        teamForAdding.addMember(memberToAdd);
+
+        return String.format(MEMBER_ADDED_SUCCESSFULLY, memberName, teamName);
     }
 }
