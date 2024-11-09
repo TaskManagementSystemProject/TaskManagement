8bb28847e7c6a141db56c5be5e88bec1650f2e1b
Danny <jordantsolov@gmail.com>
2024-11-10 00:08:37 +0200

    implement ChangeBugStatusCommand

--- src/com/oop/taskmanagement/commands/changing/ChangeBugStatusCommand.java
+++ src/com/oop/taskmanagement/commands/changing/ChangeBugStatusCommand.java
@@ -2,10 +2,18 @@ package com.oop.taskmanagement.commands.changing;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.models.contracts.tasks.Bug;
+import com.oop.taskmanagement.models.enums.SeverityType;
+import com.oop.taskmanagement.models.enums.StatusType;
+import com.oop.taskmanagement.utils.ParsingHelpers;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class ChangeBugStatusCommand implements Command {
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+    private static final String BUG_ID_PARSING_ERROR = "Bug ID must be a number!";
+    private static final String BUG_STATUS_CHANGE_SUCCESS_MESSAGE = "Bug with ID %d status changed to %s successfully.";
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -15,7 +23,14 @@ public class ChangeBugStatusCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
-        return "";
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        int bugId = ParsingHelpers.tryParseInt(parameters.get(0), BUG_ID_PARSING_ERROR);
+        StatusType newStatus = ParsingHelpers.tryParseEnum(parameters.get(1), StatusType.class);
+
+        Bug toModify = taskManagementRepository.findBugById(bugId);
+        toModify.changeStatus(newStatus);
+
+        return String.format(BUG_STATUS_CHANGE_SUCCESS_MESSAGE, bugId, newStatus);
     }
 }
