50b20b12bf124d6b2bc3e0099beca66fca5bee29
Danny <jordantsolov@gmail.com>
2024-11-09 22:39:23 +0200

    prepare ChangeBugCommand

--- src/com/oop/taskmanagement/commands/changing/ChangeBugPriorityCommand.java
+++ src/com/oop/taskmanagement/commands/changing/ChangeBugPriorityCommand.java
@@ -2,10 +2,17 @@ package com.oop.taskmanagement.commands.changing;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.models.contracts.tasks.Bug;
+import com.oop.taskmanagement.models.contracts.tasks.TaskBase;
+import com.oop.taskmanagement.utils.ParsingHelpers;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class ChangeBugPriorityCommand implements Command {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+    private static final String TASK_ID_PARSING_ERROR = "Task ID must be a number!";
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -15,7 +22,8 @@ public class ChangeBugPriorityCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
         return "";
     }
 }
