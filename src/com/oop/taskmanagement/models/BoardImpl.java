cf1fed7364615a64fd8c169ab5ef7b14f4383d13
Danny <jordantsolov@gmail.com>
2024-11-09 11:24:31 +0200

    implement BoardImpl

--- src/com/oop/taskmanagement/models/BoardImpl.java
+++ src/com/oop/taskmanagement/models/BoardImpl.java
@@ -1,6 +1,21 @@
 package com.oop.taskmanagement.models;
 
-public class BoardImpl {
-    //TODO
+import com.oop.taskmanagement.utils.ValidationHelpers;
+
+
+public class BoardImpl extends TeamAssetImpl{
+
+    private static final int NAME_MIN_LENGTH = 5;
+    private static final int NAME_MAX_LENGTH = 10;
+    private static final String NAME_LENGTH_ERROR = "Board name should be between 5 and 10 symbols.";
+
+    public BoardImpl(String name){
+        super(name);
+    }
+
+    @Override
+    protected void validateName(String name) {
+        ValidationHelpers.validateIntRange(name.length(), NAME_MIN_LENGTH, NAME_MAX_LENGTH, NAME_LENGTH_ERROR);
+    }
 
 }
