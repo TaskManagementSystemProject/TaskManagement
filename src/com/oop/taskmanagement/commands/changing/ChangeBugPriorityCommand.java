d5f7eff79bdc63a016229861c6fc3ca1ddcba877
Danny <jordantsolov@gmail.com>
2024-11-10 00:05:08 +0200

    implement ChangeBugPriorityCommand

--- src/com/oop/taskmanagement/commands/changing/ChangeBugPriorityCommand.java
+++ src/com/oop/taskmanagement/commands/changing/ChangeBugPriorityCommand.java
@@ -4,6 +4,7 @@ import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
 import com.oop.taskmanagement.models.contracts.tasks.Bug;
 import com.oop.taskmanagement.models.contracts.tasks.TaskBase;
+import com.oop.taskmanagement.models.enums.PriorityType;
 import com.oop.taskmanagement.utils.ParsingHelpers;
 import com.oop.taskmanagement.utils.ValidationHelpers;
 
@@ -12,7 +13,8 @@ import java.util.List;
 public class ChangeBugPriorityCommand implements Command {
 
     public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
-    private static final String TASK_ID_PARSING_ERROR = "Task ID must be a number!";
+    private static final String BUG_ID_PARSING_ERROR = "Bug ID must be a number!";
+    private static final String BUG_PRIORITY_CHANGE_SUCCESS_MESSAGE = "Bug with ID %d status changed to %s successfully.";
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -24,6 +26,12 @@ public class ChangeBugPriorityCommand implements Command {
     public String execute(List<String> parameters) {
         ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
 
-        return "";
+        int bugId = ParsingHelpers.tryParseInt(parameters.get(0), BUG_ID_PARSING_ERROR);
+        PriorityType newPriority = ParsingHelpers.tryParseEnum(parameters.get(1), PriorityType.class);
+
+        Bug toModify = taskManagementRepository.findBugById(bugId);
+        toModify.changePriority(newPriority);
+
+        return String.format(BUG_PRIORITY_CHANGE_SUCCESS_MESSAGE, bugId, newPriority);
     }
 }
