371ab67e736aedf8cb98f8cce253204a5b0dd988
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-09 23:11:44 +0200

    implementing command UnassignFromMemberCommand

--- src/com/oop/taskmanagement/commands/assigning/UnassignFromMemberCommand.java
+++ src/com/oop/taskmanagement/commands/assigning/UnassignFromMemberCommand.java
@@ -2,10 +2,21 @@ package com.oop.taskmanagement.commands.assigning;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.exceptions.InvalidUserInputException;
+import com.oop.taskmanagement.models.contracts.tasks.TaskBase;
+import com.oop.taskmanagement.models.contracts.team.Member;
+import com.oop.taskmanagement.models.contracts.team.TeamAsset;
+import com.oop.taskmanagement.utils.ParsingHelpers;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class UnassignFromMemberCommand implements Command {
+
+    private final static String TASK_UNASSIGNED_SUCCESSFULLY = "Task with ID %d unassigned from member %s successfully.";
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+    private static final String TASK_ID_PARSING_ERROR = "Task ID must be a number!";
+    public static final String SET_MEMBER_DOESNT_HAVE_SET_TASK_EXC_MSG = "Member: %s - doesn't have Task with id: > %d < assigned to him";
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -15,7 +26,24 @@ public class UnassignFromMemberCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
-        return "";
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        int taskId = ParsingHelpers.tryParseInt(parameters.get(0), TASK_ID_PARSING_ERROR);
+        String memberName = parameters.get(1);
+
+        // find the Task which has to be unassigned.  checks all boards + members in all teams
+        TaskBase taskToUnassign = taskManagementRepository.findTaskByIdLooping(taskId);
+        Member memberToRemoveTaskFrom = taskManagementRepository.findMemberByName(memberName);
+
+       /*   if the given name exists and the id is valid but the task
+            is not assigned to the given member throw exception     */
+        if (memberToRemoveTaskFrom.getTasks().contains(taskToUnassign)) {
+            memberToRemoveTaskFrom.removeTask(taskToUnassign);
+        } else {
+            throw new InvalidUserInputException(String.format(SET_MEMBER_DOESNT_HAVE_SET_TASK_EXC_MSG,
+                    memberToRemoveTaskFrom.getName(), taskToUnassign.getId()));
+        }
+
+        return String.format(TASK_UNASSIGNED_SUCCESSFULLY, taskId, memberToRemoveTaskFrom);
     }
 }
