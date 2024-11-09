65e6db10dbe7e2af8b96c00e9fe06b1a61fe8853
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-09 23:36:45 +0200

    implementing ChangeFeedbackStatusCommand with ugly cast /fix later/

--- src/com/oop/taskmanagement/commands/changing/ChangeFeedbackStatusCommand.java
+++ src/com/oop/taskmanagement/commands/changing/ChangeFeedbackStatusCommand.java
@@ -2,10 +2,20 @@ package com.oop.taskmanagement.commands.changing;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.models.contracts.tasks.Feedback;
+import com.oop.taskmanagement.models.contracts.tasks.TaskBase;
+import com.oop.taskmanagement.models.contracts.team.Member;
+import com.oop.taskmanagement.models.enums.StatusType;
+import com.oop.taskmanagement.utils.ParsingHelpers;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class ChangeFeedbackStatusCommand implements Command {
+    private final static String FEEDBACK_CHANGED_SUCCESSFULLY = "Feedback status changed successfully.";
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+    private static final String TASK_ID_PARSING_ERROR = "Task ID must be a number!";
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -15,7 +25,14 @@ public class ChangeFeedbackStatusCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
-        return "";
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        int feedbackId = ParsingHelpers.tryParseInt(parameters.get(0), TASK_ID_PARSING_ERROR);
+        StatusType newStatus = ParsingHelpers.tryParseEnum(parameters.get(1), StatusType.class);
+
+        Feedback feedbackToChangeStatus = (Feedback) taskManagementRepository.findTaskByIdLooping(feedbackId);
+        feedbackToChangeStatus.changeStatus(newStatus);
+
+        return String.format(FEEDBACK_CHANGED_SUCCESSFULLY);
     }
 }
