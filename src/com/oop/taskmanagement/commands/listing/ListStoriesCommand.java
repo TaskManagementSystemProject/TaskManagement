f23646b99437641c9e0fb46e30cfa70102381aac
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-10 15:33:37 +0200

    implementing ListStoriesCommand

--- src/com/oop/taskmanagement/commands/listing/ListStoriesCommand.java
+++ src/com/oop/taskmanagement/commands/listing/ListStoriesCommand.java
@@ -1,11 +1,22 @@
 package com.oop.taskmanagement.commands.listing;
 
 import com.oop.taskmanagement.commands.contracts.Command;
+import com.oop.taskmanagement.commands.enums.FilterType;
+import com.oop.taskmanagement.commands.enums.ListingType;
+import com.oop.taskmanagement.commands.enums.SortType;
+import com.oop.taskmanagement.commands.listing.utility.FilteringAndSortingHelperMethods;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.exceptions.InvalidUserInputException;
+import com.oop.taskmanagement.models.contracts.tasks.Story;
+import com.oop.taskmanagement.utils.ParsingHelpers;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListStoriesCommand implements Command {
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS_SORTING = 2;
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -15,7 +26,41 @@ public class ListStoriesCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
-        return "";
+        if (parameters.isEmpty()) {
+            return taskManagementRepository.getStories().toString();
+        }
+
+
+        ListingType listingType = ParsingHelpers.tryParseEnum(parameters.get(0), ListingType.class);
+
+        return switch (listingType) {
+            case FILTER -> {
+                FilterType filterType = ParsingHelpers.tryParseEnum(parameters.get(1), FilterType.class);
+                yield FilteringAndSortingHelperMethods.filterTasks(taskManagementRepository, parameters, filterType);
+            }
+            case SORT -> {
+                ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS_SORTING);
+                SortType sortType = ParsingHelpers.tryParseEnum(parameters.get(1), SortType.class);
+                yield sortTasks(getComparator(sortType));
+            }
+        };
+    }
+
+    private Comparator<Story> getComparator(SortType sortType) {
+        return switch (sortType) {
+            case TITLE -> Comparator.comparing(Story::getTitle);
+            case PRIORITY -> Comparator.comparing(Story::getPriority);
+            case SIZE -> Comparator.comparing(Story::getSize);
+            default ->
+                    throw new InvalidUserInputException(String.format("Feedback cannot be sorted by field %s ", sortType));
+        };
+    }
+
+    private String sortTasks(Comparator<Story> storyComparator) {
+        return taskManagementRepository.getStories()
+                .stream()
+                .sorted(storyComparator)
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
     }
 }
