e31d448143d83c53e93ead70a26f0c8eb46273a0
Danny <jordantsolov@gmail.com>
2024-11-10 09:47:58 +0200

    implement CreateBoardInTeamCommand

--- src/com/oop/taskmanagement/commands/creation/CreateBoardInTeamCommand.java
+++ src/com/oop/taskmanagement/commands/creation/CreateBoardInTeamCommand.java
@@ -2,10 +2,17 @@ package com.oop.taskmanagement.commands.creation;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.models.contracts.team.Board;
+import com.oop.taskmanagement.models.contracts.team.Team;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class CreateBoardInTeamCommand implements Command {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+
+    private static final String CREATE_BOARD__SUCCESS_MESSAGE = "Board %s created successfully in team %s";
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -15,7 +22,14 @@ public class CreateBoardInTeamCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
-        return "";
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        String boardName = parameters.get(0);
+        String teamName = parameters.get(1);
+        Team teamToAddBoardIn = taskManagementRepository.findTeamByName(teamName);
+        Board newBoard = taskManagementRepository.createBoardInTeam(boardName, teamToAddBoardIn);
+        newBoard.logActivity(String.format("Board %s added to team %s", boardName, teamName));
+
+        return String.format(CREATE_BOARD__SUCCESS_MESSAGE, boardName, teamName);
     }
 }
