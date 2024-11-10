2fa7d119673ccbae3189be1c44bb409c1d1bbd96
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-10 11:37:28 +0200

    implementing CreateTeamCommand

--- src/com/oop/taskmanagement/commands/creation/CreateTeamCommand.java
+++ src/com/oop/taskmanagement/commands/creation/CreateTeamCommand.java
@@ -2,10 +2,14 @@ package com.oop.taskmanagement.commands.creation;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class CreateTeamCommand implements Command {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
+    private static final String CREATE_TEAM_SUCCESS_MESSAGE = "Team %s created successfully";
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -15,7 +19,11 @@ public class CreateTeamCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
-        return "";
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        String teamName = parameters.getFirst();
+        taskManagementRepository.createTeam(teamName);
+
+        return String.format(CREATE_TEAM_SUCCESS_MESSAGE, teamName);
     }
 }
