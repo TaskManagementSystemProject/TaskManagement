82e60a5c6116fff95cc862d1e1c587b31fbf3097
Danny <jordantsolov@gmail.com>
2024-11-10 13:33:47 +0200

    implement ShowTeamActivityCommand

--- src/com/oop/taskmanagement/commands/showing/ShowTeamActivityCommand.java
+++ src/com/oop/taskmanagement/commands/showing/ShowTeamActivityCommand.java
@@ -2,10 +2,16 @@ package com.oop.taskmanagement.commands.showing;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.models.contracts.team.Team;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ShowTeamActivityCommand implements Command {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -15,7 +21,14 @@ public class ShowTeamActivityCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
-        return "";
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        String teamName = parameters.get(0);
+        Team team = taskManagementRepository.findTeamByName(teamName);
+
+        return Stream.concat(team.getMembers().stream(), team.getBoards().stream())
+                .flatMap(teamAsset -> teamAsset.getActivityHistory().stream())
+                .collect(Collectors.joining(", "));
     }
+
 }
