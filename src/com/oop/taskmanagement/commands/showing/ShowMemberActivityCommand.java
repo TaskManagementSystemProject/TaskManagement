d4de832b22a42b1045c0d5a7fff37282b64a61c5
Danny <jordantsolov@gmail.com>
2024-11-10 13:22:24 +0200

    implement ShowMemberActivityCommand

--- src/com/oop/taskmanagement/commands/showing/ShowMemberActivityCommand.java
+++ src/com/oop/taskmanagement/commands/showing/ShowMemberActivityCommand.java
@@ -2,10 +2,14 @@ package com.oop.taskmanagement.commands.showing;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.models.contracts.team.Member;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class ShowMemberActivityCommand implements Command {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -15,7 +19,11 @@ public class ShowMemberActivityCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
-        return "";
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        String memberName = parameters.get(0);
+        Member member = taskManagementRepository.findMemberByName(memberName);
+
+        return String.join(", ", member.getActivityHistory());
     }
 }
