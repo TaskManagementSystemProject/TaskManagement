f1c191b38e7c616e227bc7c17f881855b2e62d4b
Danny <jordantsolov@gmail.com>
2024-11-10 22:10:58 +0200

    implement BugImplTest

---
+++ tests/com/oop/taskmanagement/models/tasks/BugImplTest.java
@@ -0,0 +1,256 @@
+package com.oop.taskmanagement.models.tasks;
+
+import com.oop.taskmanagement.exceptions.InvalidUserInputException;
+import com.oop.taskmanagement.models.contracts.tasks.Bug;
+import com.oop.taskmanagement.models.contracts.tasks.Comment;
+import com.oop.taskmanagement.models.contracts.tasks.TaskBase;
+import com.oop.taskmanagement.models.enums.PriorityType;
+import com.oop.taskmanagement.models.enums.SeverityType;
+import com.oop.taskmanagement.models.enums.StatusType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BugImplTest {
+
+    private static final String VALID_TITLE = "10symb tit";
+    private static final String VALID_DESCRIPTION = "10symb des";
+    private static final List<String> VALID_LIST_TO_REPRODUCE = new ArrayList<>(Arrays.asList("Step1","Step2"));
+    private int currentId = 1;
+
+    @Test
+    public void BugImpl_Should_ImplementBugInterface(){
+        // Arrange, Act
+        BugImpl bug = initializeValidBug();
+
+        // Assert
+        Assertions.assertTrue(bug instanceof Bug);
+    }
+
+    @Test
+    public void BugImpl_Should_ImplementTaskBaseInterface(){
+        // Arrange, Act
+        TaskBase bug = initializeValidBug();
+
+        // Assert
+        Assertions.assertTrue(bug instanceof TaskBase);
+    }
+
+    @Test
+    public void constructor_Should_ThrowException_When_InvalidNameProvided(){
+        // Arrange, Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new BugImpl(currentId,
+                        "Invalid",
+                        VALID_DESCRIPTION,
+                        VALID_LIST_TO_REPRODUCE,
+                        PriorityType.LOW,
+                        SeverityType.MINOR));
+    }
+
+    @Test
+    public void constructor_Should_ThrowException_When_InvalidDescriptionProvided(){
+        // Arrange, Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new BugImpl(currentId,
+                        VALID_TITLE,
+                        "Invalid",
+                        VALID_LIST_TO_REPRODUCE,
+                        PriorityType.LOW,
+                        SeverityType.MINOR));
+    }
+
+    @Test
+    public void constructor_Should_createNewBug_When_ArgumentsAreValid() {
+        // Arrange, Act
+        Bug bug = initializeValidBug();
+
+        // Assert
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(currentId, bug.getId()),
+                () -> Assertions.assertEquals(VALID_TITLE, bug.getTitle()),
+                () -> Assertions.assertEquals(VALID_DESCRIPTION, bug.getDescription()),
+                () -> Assertions.assertEquals(VALID_LIST_TO_REPRODUCE, bug.getStepsToReproduce()),
+                () -> Assertions.assertEquals(PriorityType.LOW, bug.getPriority()),
+                () -> Assertions.assertEquals(SeverityType.MINOR, bug.getSeverity()),
+                () -> Assertions.assertEquals(StatusType.ACTIVE, bug.getStatus())
+        );
+    }
+
+    @Test
+    public void setAssignee_Should_AssignName(){
+        // Arrange
+        Bug bug = initializeValidBug();
+        String expectedOutput = "Gosho";
+        // Act
+        bug.setAssigneeName("Gosho");
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, bug.getAssigneeName());
+    }
+    @Test
+    public void changeStatus_Should_ThrowException_When_InvalidStatusProvided(){
+        // Arrange
+        Bug bug = initializeValidBug();
+
+        // Act, Assert
+        Assertions.assertThrows(InvalidUserInputException.class, () -> bug.changeStatus(StatusType.IN_PROGRESS));
+    }
+
+    @Test
+    public void changeStatus_Should_ChangeStatus_When_ValidStatusProvided(){
+        // Arrange
+        Bug bug = initializeValidBug();
+
+        // Act
+        bug.changeStatus(StatusType.DONE);
+
+        // Assert
+        Assertions.assertEquals(StatusType.DONE, bug.getStatus());
+    }
+
+    @Test
+    public void changeStatus_Should_LogChange_When_ValidStatusProvided(){
+        // Arrange
+        Bug bug = initializeValidBug();
+        String expectedOutput = String.format("Status changed from %s to %s.", StatusType.ACTIVE, StatusType.DONE);
+        // Act
+        bug.changeStatus(StatusType.DONE);
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, bug.getEventLog().get(0));
+    }
+
+    @Test
+    public void changeSeverity_Should_ChangeSeverity(){
+        // Arrange
+        Bug bug = initializeValidBug();
+
+        // Act
+        bug.changeSeverity(SeverityType.MAJOR);
+
+        // Assert
+        Assertions.assertEquals(SeverityType.MAJOR, bug.getSeverity());
+    }
+
+    @Test
+    public void changeSeverity_Should_LogChange_When_ValidSeverityProvided(){
+        // Arrange
+        Bug bug = initializeValidBug();
+        String expectedOutput = String.format("Severity changed from %s to %s.", SeverityType.MINOR, SeverityType.MAJOR);
+        // Act
+        bug.changeSeverity(SeverityType.MAJOR);
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, bug.getEventLog().get(0));
+    }
+
+    @Test
+    public void changePriority_Should_ChangePriority(){
+        // Arrange
+        Bug bug = initializeValidBug();
+
+        // Act
+        bug.changePriority(PriorityType.HIGH);
+
+        // Assert
+        Assertions.assertEquals(PriorityType.HIGH, bug.getPriority());
+    }
+
+    @Test
+    public void changePriority_Should_LogChange_When_ValidPriorityProvided(){
+        // Arrange
+        Bug bug = initializeValidBug();
+        String expectedOutput = String.format("Priority changed from %s to %s.", PriorityType.LOW, PriorityType.HIGH);
+        // Act
+        bug.changePriority(PriorityType.HIGH);
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, bug.getEventLog().get(0));
+    }
+
+    @Test
+    public void toString_Should_ConvertToStringWithProperFormat(){
+        // Arrange
+        Bug bug = initializeValidBug();
+        String expectedResult = String.format("Task with id: %d%nTitle: %s%nStatus: %s%n" +
+                        "Priority: %s%nSeverity: %s%nAssigned to: %s",
+                currentId,
+                VALID_TITLE,
+                StatusType.ACTIVE,
+                PriorityType.LOW,
+                SeverityType.MINOR,
+                null
+                );
+        // Act
+        String actualOutput = bug.toString();
+
+        // Assert
+        Assertions.assertEquals(expectedResult,actualOutput);
+    }
+
+    @Test
+    public void addComment_Should_AddCommentInList(){
+        // Arrange
+        Bug bug = initializeValidBug();
+        Comment comment = new CommentImpl("Pesho","Long message");
+
+        // Act
+        bug.addComment(comment);
+
+        // Assert
+        Assertions.assertEquals(1,bug.getComments().size());
+    }
+
+    @Test
+    public void addComment_Should_LogAdding(){
+        // Arrange
+        Bug bug = initializeValidBug();
+        Comment comment = new CommentImpl("Pesho","Long message");
+        String expectedLog = "Comment added successfully";
+        // Act
+        bug.addComment(comment);
+
+        // Assert
+        Assertions.assertEquals(expectedLog,bug.getEventLog().get(0));
+    }
+    @Test
+    public void getEventLog_Should_ReturnListOfLogs(){
+        // Arrange
+        Bug bug = initializeValidBug();
+        bug.addComment(new CommentImpl("Gosho","Some message"));
+        bug.changePriority(PriorityType.HIGH);
+        List<String> expectedOutput = new ArrayList<>(
+                Arrays.asList("Comment added successfully",
+                        String.format("Priority changed from %s to %s.", PriorityType.LOW, PriorityType.HIGH)));
+        // Act
+        List<String> actualOutput = bug.getEventLog();
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void getComments_Should_ReturnListOfComments(){
+        // Arrange
+        Bug bug = initializeValidBug();
+        Comment firstComment = new CommentImpl("Gosho","Some message");
+        Comment secondComment = new CommentImpl("Pesho", "Other message");
+        bug.addComment(firstComment);
+        bug.addComment(secondComment);
+        List<Comment> expectedOutput = new ArrayList<>(Arrays.asList(firstComment,secondComment));
+
+        // Act
+        List<Comment> actualOutput = bug.getComments();
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    private BugImpl initializeValidBug(){
+        return new BugImpl(currentId, VALID_TITLE, VALID_DESCRIPTION, VALID_LIST_TO_REPRODUCE,PriorityType.LOW, SeverityType.MINOR);
+    }
+}
