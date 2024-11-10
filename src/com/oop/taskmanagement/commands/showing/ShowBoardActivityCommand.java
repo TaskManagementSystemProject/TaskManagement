6e16a04cdccfbb7498b673e5983171dda010ac2d
Danny <jordantsolov@gmail.com>
2024-11-10 13:20:31 +0200

    implement ShowBoardActivityCommand

--- src/com/oop/taskmanagement/commands/showing/ShowBoardActivityCommand.java
+++ src/com/oop/taskmanagement/commands/showing/ShowBoardActivityCommand.java
@@ -2,10 +2,14 @@ package com.oop.taskmanagement.commands.showing;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.models.contracts.team.Board;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class ShowBoardActivityCommand implements Command {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -15,7 +19,13 @@ public class ShowBoardActivityCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        // TODO
-        return "";
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        String boardName = parameters.get(0);
+        String teamName = parameters.get(1);
+
+        Board board = taskManagementRepository.findBoardByTeamName(boardName, teamName);
+
+        return String.join(", ", board.getActivityHistory());
     }
 }
