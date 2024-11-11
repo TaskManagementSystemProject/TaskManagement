087c9246b3373fa7155399f2c8332764a382405b
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-11-11 15:57:27 +0200

    adding Constants for CommentImplTest

--- tests/com/oop/taskmanagement/utils/Constants.java
+++ tests/com/oop/taskmanagement/utils/Constants.java
@@ -6,22 +6,31 @@ import java.util.List;
 
 public class Constants {
 
-    public static final String VALID_TEAM_NAME = "Otbor";
-    public static final String VALID_COMMENT_AUTHOR = "JohnSmith";
-    public static final String VALID_COMMENT_MESSAGE = "Messages can be from 5 to 500 characters";
-    public static final String VALID_TEAM_NAME_TWO = "Otbor1";
-    public static final String VALID_BOARD_NAME = "White";
-    public static final String VALID_MEMBER_NAME_ONE = "Gosho";
-    public static final String VALID_MEMBER_NAME_TWO = "Pesho";
+    // Tasks
+    public static int currentId = 1;
     public static final String VALID_TITLE = "10symb tit";
     public static final String VALID_DESCRIPTION = "10symb des";
     public static final List<String> VALID_LIST_TO_REPRODUCE = new ArrayList<>(Arrays.asList("Step1","Step2"));
     public static final int VALID_RATING = 5;
-    public static int currentId = 1;
+    // Team
+    public static final String VALID_TEAM_NAME = "Otbor";
+    public static final String VALID_TEAM_NAME_TWO = "Otbor1";
     public static final String INVALID_TEAM_SHORT_NAME = "Geor";
     public static final String INVALID_TEAM_LONG_NAME = "GeorgiYordanOtbo";
+    // Board
+    public static final String VALID_BOARD_NAME = "White";
     public static final String INVALID_BOARD_SHORT_NAME = "Bob";
     public static final String INVALID_BOARD_LONG_NAME = "DaveTheMagicalCheeseWizard";
+    // Member
+    public static final String VALID_MEMBER_NAME_ONE = "Gosho";
+    public static final String VALID_MEMBER_NAME_TWO = "Pesho";
     public static final String INVALID_MEMBER_SHORT_NAME = "Bob";
     public static final String INVALID_MEMBER_LONG_NAME = "DaveTheMagicalCheeseWizard";
+    // Comments
+    public static final String VALID_COMMENT_AUTHOR = "JohnSmith"; // 5 - 15
+    public static final String VALID_COMMENT_MESSAGE = "Messages can be from 5 to 500 characters";
+    public static final String INVALID_COMMENT_SHORT_MESSAGE = "XXXX";
+    public static final String INVALID_COMMENT_LONG_MESSAGE = "X".repeat(501);
+    public static final String INVALID_AUTHOR_SHORT_NAME = "Bob";  // 5 - 15
+    public static final String INVALID_AUTHOR_LONG_NAME = "DaveTheMagicalCheeseWizard";
 }
