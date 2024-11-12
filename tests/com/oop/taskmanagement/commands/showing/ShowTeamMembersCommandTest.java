da126b38a3e66a635c4c3aa21bf86868e9f9e11d
Danny <jordantsolov@gmail.com>
2024-11-12 09:35:48 +0200

    implement ShowTeamMembersCommandTest

--- tests/com/oop/taskmanagement/commands/showing/ShowTeamMembersCommandTest.java
+++ tests/com/oop/taskmanagement/commands/showing/ShowTeamMembersCommandTest.java
@@ -1,4 +1,66 @@
 package com.oop.taskmanagement.commands.showing;
 
+import com.oop.taskmanagement.commands.contracts.Command;
+import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.exceptions.InvalidUserInputException;
+import com.oop.taskmanagement.models.contracts.team.Team;
+import com.oop.taskmanagement.utils.ValidInitialization;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class ShowTeamMembersCommandTest {
+    private TaskManagementRepository repository;
+    private Command showTeamMembers;
+
+
+    @BeforeEach
+    public void initialize() {
+        repository = ValidInitialization.createDummyRepository();
+        showTeamMembers = new ShowTeamMembersCommand(repository);
+    }
+
+    @Test
+    public void execute_Should_ThrowException_When_ArgumentsCountDiffer(){
+        // Arrange, Act, Assert
+        Assertions.assertThrows(InvalidUserInputException.class, () -> showTeamMembers.execute(List.of()));
+    }
+
+    @Test
+    public void execute_Should_ThrowException_When_TeamNotFound(){
+        // Arrange, Act, Assert
+        Assertions.assertThrows(InvalidUserInputException.class, () -> showTeamMembers.execute(List.of("INVALID")));
+    }
+
+    @Test
+    public void execute_Should_ReturnMembersOfTeamAsString_When_ValidArguments(){
+        // Arrange
+        String expectedOutput = String.format("Name: Pesho, type: member.%nName: TestMember, type: member.");
+        repository.createMember("TestMember");
+        Team team = repository.findTeamByName("Otbor1");
+        team.addMember(repository.findMemberByName("TestMember"));
+
+        // TODO for all listings to put some counter in front, to 1. data then 2.data etc
+
+        // Act
+        String actualOutput = showTeamMembers.execute(List.of("Otbor1"));
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void execute_Should_ReturnEmptyStringWhenNoBoardsOfTeam_When_ValidArguments(){
+        // Arrange
+        String expectedOutput = "";
+        repository.createTeam("Otbor2");
+
+        // Act
+        String actualOutput = showTeamMembers.execute(List.of("Otbor2"));
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
 }
