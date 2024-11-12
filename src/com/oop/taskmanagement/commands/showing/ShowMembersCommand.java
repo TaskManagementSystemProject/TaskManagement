4aeac2c8e6f0001608a1ae0f65d7b4e7f3348c38
Danny <jordantsolov@gmail.com>
2024-11-12 09:43:37 +0200

    implement ShowTeamsCommandTest

--- src/com/oop/taskmanagement/commands/showing/ShowMembersCommand.java
+++ src/com/oop/taskmanagement/commands/showing/ShowMembersCommand.java
@@ -2,11 +2,13 @@ package com.oop.taskmanagement.commands.showing;
 
 import com.oop.taskmanagement.commands.contracts.Command;
 import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ShowMembersCommand implements Command {
+    private static final int EXPECTED_NUMBER_OF_ARGUMENTS = 0;
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -16,6 +18,7 @@ public class ShowMembersCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
 
         return taskManagementRepository.getMembers()
                 .stream()
--- tests/com/oop/taskmanagement/commands/showing/ShowTeamsCommandTest.java
+++ tests/com/oop/taskmanagement/commands/showing/ShowTeamsCommandTest.java
@@ -1,4 +1,43 @@
 package com.oop.taskmanagement.commands.showing;
 
+import com.oop.taskmanagement.commands.contracts.Command;
+import com.oop.taskmanagement.core.contracts.TaskManagementRepository;
+import com.oop.taskmanagement.exceptions.InvalidUserInputException;
+import com.oop.taskmanagement.utils.ValidInitialization;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class ShowTeamsCommandTest {
+
+    private TaskManagementRepository repository;
+    private Command showTeams;
+
+    @BeforeEach
+    public void initialize() {
+        repository = ValidInitialization.createDummyRepository();
+        showTeams = new ShowTeamsCommand(repository);
+    }
+
+    @Test
+    public void execute_Should_ThrowException_When_ArgumentsCountDiffer(){
+        // Arrange, Act, Assert
+        Assertions.assertThrows(InvalidUserInputException.class, () -> showTeams.execute(List.of("EXTRA ARGUMENTS")));
+    }
+
+    @Test
+    public void execute_Should_ReturnTeamsAsString_When_ValidArguments(){
+        // Arrange
+        String expectedOutput = String.format("Name: Otbor, type: team.%nName: Otbor1, type: team%nName: TestTeam, type: team.");
+        repository.createTeam("TestTeam");
+
+        // Act
+        String actualOutput = showTeams.execute(List.of());
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, actualOutput);
+
+    }
 }
