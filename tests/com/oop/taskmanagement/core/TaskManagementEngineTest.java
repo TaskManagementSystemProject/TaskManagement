2abf46003f4448d09ba9a6165a059c010ed56eff
Danny <jordantsolov@gmail.com>
2024-11-13 18:59:16 +0200

    implement TaskManagementEngineTest from files

---
+++ tests/com/oop/taskmanagement/core/TaskManagementEngineTest.java
@@ -0,0 +1,78 @@
+package com.oop.taskmanagement.core;
+
+import com.oop.taskmanagement.core.contracts.TaskManagementEngine;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class TaskManagementEngineTest {
+    private TaskManagementEngine taskManagementEngine;
+
+    @BeforeEach
+    public void initialize(){
+        taskManagementEngine = new TaskManagementEngineImpl();
+    }
+    @Test
+    public void start_Should_MatchFirstSampleOutput_When_FirstSampleInputProvided() throws IOException {
+        // Arrange
+        File inputFile = new File("resources/SampleCase1/firstSampleInput.txt");
+        System.setIn(new FileInputStream(inputFile)); // redirects the System.in to read from file rather than the console
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); // redirect System.out to outputStream
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        String expectedOutput = Files.readString(Paths.get("resources/SampleCase1/firstSampleOutput.txt")); // get the expected output from the file
+
+        // Act
+        taskManagementEngine.start();
+        String actualOutput = outputStream.toString();
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void start_Should_MatchSecondSampleOutput_When_SecondSampleInputProvided() throws IOException {
+        // Arrange
+        File inputFile = new File("resources/SampleCase2/secondSampleInput.txt");
+        System.setIn(new FileInputStream(inputFile)); // redirects the System.in to read from file rather than the console
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); // redirect System.out to outputStream
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        String expectedOutput = Files.readString(Paths.get("resources/SampleCase2/secondSampleOutput.txt")); // get the expected output from the file
+
+        // Act
+        taskManagementEngine.start();
+        String actualOutput = outputStream.toString();
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void start_Should_MatchThirdSampleOutput_When_ThirdSampleInputProvided() throws IOException {
+        // Arrange
+        File inputFile = new File("resources/SampleCase3/thirdSampleInput.txt");
+        System.setIn(new FileInputStream(inputFile)); // redirects the System.in to read from file rather than the console
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); // redirect System.out to outputStream
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        String expectedOutput = Files.readString(Paths.get("resources/SampleCase3/thirdSampleOutput.txt")); // get the expected output from the file
+
+        // Act
+        taskManagementEngine.start();
+        String actualOutput = outputStream.toString();
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+}
